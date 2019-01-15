d73a9bef0935e92065c64ae27e3cfdbe13771ba0
Whiteblock <hello@whiteblock.io>
2019-01-15 23:30:15 +0000

    fix default

--- blockchains/rchain/conf.go
+++ blockchains/rchain/conf.go
@@ -141,6 +141,6 @@ func GetDefaults() string {
     "validatorCount":5,
     "validators":0,
     "sigAlgorithm":"ed25519",
-    "command":"/opt/docker/bin/rnode"
+    "command":"/rchain/node/target/rnode-0.8.2/usr/share/rnode/bin/rnode"
 }`
 }
