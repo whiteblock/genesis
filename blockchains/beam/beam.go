19cf5c633c4af8702fdd2fe209e91781033bbd55
Daniel Choi <daniel@Daniels-MacBook-Pro.local>
2019-01-30 12:38:07 -0800

    got library to work now. Good to go

--- blockchains/beam/beam.go
+++ blockchains/beam/beam.go
@@ -31,7 +31,7 @@ func Build(data map[string]interface{}, nodes int, servers []db.Server, clients 
 	secretMinerKeys := []string{}
 	// walletIDs := []string{}
 	for i, server := range servers {
-		for localId, ip := range server.Ips {
+		for localId, _ := range server.Ips {
 			_, err := clients[i].DockerExec(localId, "beam-wallet --command init --pass password")
 			if err != nil {
 				// log.Println(err)
@@ -55,8 +55,6 @@ func Build(data map[string]interface{}, nodes int, servers []db.Server, clients 
 			re = regexp.MustCompile(`(?m)^Secret([A-z|0-9|\s|\:|\/|\+|\=])*$`)
 			secMLine := re.FindAllString(res2, -1)[0]
 			secretMinerKeys = append(secretMinerKeys, strings.Split(secMLine, " ")[3])
-
-			clients[i].DockerExecd(localId, fmt.Sprintf("bash -c 'beam-wallet --command listen -n %s:%d --pass password 2>&1 >> /beam/walletID.log'", ip, port))
 		}
 	}
 
@@ -71,7 +69,7 @@ func Build(data map[string]interface{}, nodes int, servers []db.Server, clients 
 	node := 0
 	for i, server := range servers {
 		for range server.Ips {
-			config := []string{
+			beam_node_config := []string{
 				"# port=10000",
 				"# stratum_port=0",
 				"# stratum_secrets_path=.",
@@ -98,7 +96,23 @@ func Build(data map[string]interface{}, nodes int, servers []db.Server, clients 
 				"Maturity.Coinbase=1",
 				"# Maturity.Std=0",
 				"# MaxBodySize=0x100000",
-				"DA_Target_s=1",
+				"DA.Target_s=1",
+				"# DA.MaxAhead_s=900",
+				"# DA.WindowWork=120",
+				"# DA.WindowMedian0=25",
+				"# DA.WindowMedian1=7",
+				"DA.Difficulty0=100",
+				"# AllowPublicUtxos=0",
+				"# FakePoW=0",
+			}
+			beam_wallet_config := []string{
+				"# Emission.Value0=800000000",
+				"# Emission.Drop0=525600",
+				"# Emission.Drop1=2102400",
+				"Maturity.Coinbase=1",
+				"# Maturity.Std=0",
+				"# MaxBodySize=0x100000",
+				"DA.Target_s=1",
 				"# DA.MaxAhead_s=900",
 				"# DA.WindowWork=120",
 				"# DA.WindowMedian0=25",
@@ -108,15 +122,21 @@ func Build(data map[string]interface{}, nodes int, servers []db.Server, clients 
 				"# FakePoW=0",
 			}
 			for _, ip := range append(ips[:node], ips[node+1:]...) {
-				config = append(config, fmt.Sprintf("peer=%s:%d", ip, port))
+				beam_node_config = append(beam_node_config, fmt.Sprintf("peer=%s:%d", ip, port))
 			}
-			err := util.Write("./beam-node.cfg", util.CombineConfig(config))
+			err := util.Write("./beam-node.cfg", util.CombineConfig(beam_node_config))
+			if err != nil {
+				log.Println(err)
+				return nil, err
+			}
+			err = util.Write("./beam-wallet.cfg", util.CombineConfig(beam_wallet_config))
 			if err != nil {
 				log.Println(err)
 				return nil, err
 			}
 
 			defer util.Rm("./beam-node.cfg")
+			defer util.Rm("./beam-wallet.cfg")
 
 			err = clients[i].Scp("./beam-node.cfg", "/home/appo/beam-node.cfg")
 			if err != nil {
@@ -124,17 +144,29 @@ func Build(data map[string]interface{}, nodes int, servers []db.Server, clients 
 				return nil, err
 			}
 
-			defer clients[i].Run("rm -f /home/appo/beam-node.cfg")
+			err = clients[i].Scp("./beam-wallet.cfg", "/home/appo/beam-wallet.cfg")
+			if err != nil {
+				log.Println(err)
+				return nil, err
+			}
 
-			// directory for cp is different. change back to just /beam when using the tar dockerfile
-			_, err = clients[i].Run(fmt.Sprintf("docker cp /home/appo/beam-node.cfg %s%d:/beam/beam", conf.NodePrefix, node))
+			defer clients[i].Run("rm -f /home/appo/beam-node.cfg")
+			defer clients[i].Run("rm -f /home/appo/beam-wallet.cfg")
+
+			_, err = clients[i].Run(fmt.Sprintf("docker cp /home/appo/beam-node.cfg %s%d:/beam", conf.NodePrefix, node))
+			if err != nil {
+				log.Println(err)
+				return nil, err
+			}
+
+			_, err = clients[i].Run(fmt.Sprintf("docker cp /home/appo/beam-wallet.cfg %s%d:/beam", conf.NodePrefix, node))
 			if err != nil {
 				log.Println(err)
 				return nil, err
 			}
 
 			// will copy the treasury binary (taken from beam github). Not sure if this is necessary.
-			_, err = clients[i].Run(fmt.Sprintf("docker cp /home/appo/beam/treasury.bin %s%d:/beam/beam", conf.NodePrefix, node))
+			_, err = clients[i].Run(fmt.Sprintf("docker cp /home/appo/beam/treasury.bin %s%d:/beam", conf.NodePrefix, node))
 			if err != nil {
 				log.Println(err)
 				return nil, err
@@ -148,15 +180,25 @@ func Build(data map[string]interface{}, nodes int, servers []db.Server, clients 
 	totNodes := 0
 
 	for i, server := range servers {
-		for localId, _ := range server.Ips {
+		for localId, ip := range server.Ips {
 			if totNodes < int(beamConf.Validators) {
 				_, err := clients[i].DockerExecd(localId, "beam-node --mining_threads 1")
 				if err != nil {
 					log.Println(err)
 					return nil, err
 				}
+				_, err = clients[i].DockerExecd(localId, fmt.Sprintf("beam-wallet --command listen -n %s:%d --pass password", ip, port))
+				if err != nil {
+					log.Println(err)
+					return nil, err
+				}
 			} else if totNodes >= int(beamConf.Validators) {
 				_, err := clients[i].DockerExecd(localId, "beam-node")
+				if err != nil {
+					log.Println(err)
+					return nil, err
+				}
+				_, err = clients[i].DockerExecd(localId, fmt.Sprintf("beam-wallet --command listen -n %s:%d --pass password", ip, port))
 				if err != nil {
 					log.Println(err)
 					return nil, err
