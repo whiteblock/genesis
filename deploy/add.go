5acc7f887230bf70dd76025714ec479973c5af05
Whiteblock <hello@whiteblock.io>
2019-02-15 00:52:22 +0000

    clean up

--- deploy/add.go
+++ deploy/add.go
@@ -19,8 +19,6 @@ import(
  * @param r [description]
  * @return [description]
  */
-
-
 func AddNodes(buildConf *Config,servers []db.Server,resources []util.Resources,clients []*util.SshClient,
               buildState *state.BuildState) (map[int][]string,error) {
     
@@ -120,66 +118,4 @@ func AddNodes(buildConf *Config,servers []db.Server,resources []util.Resources,c
     
     log.Println("Finished adding nodes into the network")
     return out, buildState.GetError()
-}
-
-
-func AddNodesLegacy(buildConf *Config, servers []db.Server,resources util.Resources,
-              clients []*util.SshClient,buildState *state.BuildState) (map[int][]string,error){
-    
-    buildState.SetDeploySteps(3*buildConf.Nodes + 2 )
-    defer buildState.FinishDeploy()
-
-    var sem = semaphore.NewWeighted(conf.ThreadLimit)
-    
-    ctx := context.TODO()
-
-    fmt.Println("-------------Building The Docker Containers-------------")
-    n := buildConf.Nodes
-    i := 0
-    out := map[int][]string{}
-    for n > 0 && i < len(servers){
-        out[servers[i].Id] = []string{}
-        fmt.Printf("-------------Building on Server %d-------------\n",i)
-        
-        max_nodes := int(servers[i].Max - servers[i].Nodes)
-        var nodes int
-        if max_nodes > n {
-            nodes = n
-        }else{
-            nodes = max_nodes
-        }
-        for j := 0; j < nodes; j++ {
-            out[servers[i].Id] = append(out[servers[i].Id],util.GetNodeIP(servers[i].ServerID,j))
-        }
-        
-        buildState.SetBuildStage("Provisioning Nodes")
-       
-        err := DockerNetworkCreateAppendAll(servers[i],clients[i],servers[i].Nodes,nodes,buildState)
-        if err != nil {
-            log.Println(err)
-            return nil,err
-        }
-        
-        fmt.Printf("Creating the docker containers on server %d\n",i)
-        buildState.SetBuildStage("Configuring Network")
-        err = DockerRunAppendAll(servers[i],clients[i],resources,servers[i].Nodes,nodes,buildConf.Image,buildState)
-        if err != nil{
-            log.Println(err)
-            return nil,err
-        }
-        n -= nodes
-        i++
-
-    }
-    //Acquire all of the resources here, then release and destroy
-    err := sem.Acquire(ctx,conf.ThreadLimit)
-    if err != nil {
-        log.Println(err)
-        return out, nil
-    }
-    sem.Release(conf.ThreadLimit)
-    if n != 0 {
-        return out, errors.New(fmt.Sprintf("ERROR: Only able to build %d/%d nodes\n",(buildConf.Nodes - n),buildConf.Nodes))
-    }
-    return out, err
 }
