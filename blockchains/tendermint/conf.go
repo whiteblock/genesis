ef93fdcb1fc3f650d669588fdbe345ecc4476f4e
Whiteblock <hello@whiteblock.io>
2019-03-11 19:30:14 +0000

    switch tendermint to new format

--- blockchains/tendermint/conf.go
+++ blockchains/tendermint/conf.go
@@ -1,18 +1,25 @@
 package tendermint
 
 import(
+    "io/ioutil"
     util "../../util"
 )
 
-
-func GetDefaults() string{
-    return `{}`
+func GetParams() string {
+    dat, err := ioutil.ReadFile("./resources/tendermint/params.json")
+    if err != nil {
+        panic(err)//Missing required files is a fatal error
+    }
+    return string(dat)
 }
 
-func GetParams() string{
-    return `[]`
+func GetDefaults() string {
+    dat, err := ioutil.ReadFile("./resources/tendermint/defaults.json")
+    if err != nil {
+        panic(err)//Missing required files is a fatal error
+    }
+    return string(dat)
 }
-
 
 func GetServices() []util.Service {
     return nil
