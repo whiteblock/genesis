15881a7775402ebdbbf46bdeed92c3a45140e92d
n8wb <nate@whiteblock.io>
2019-04-11 17:38:23 +0000

    prevent bad validator amounts

--- blockchains/artemis/conf.go
+++ blockchains/artemis/conf.go
@@ -1,12 +1,12 @@
 package artemis
 
 import (
-	"fmt"
-	"log"
-	"encoding/json"
-	"io/ioutil"
-	"github.com/Whiteblock/mustache"
 	util "../../util"
+	"encoding/json"
+	"fmt"
+	"github.com/Whiteblock/mustache"
+	"io/ioutil"
+	"log"
 )
 
 type ArtemisConf map[string]interface{}
@@ -20,7 +20,13 @@ func NewConf(data map[string]interface{}) (ArtemisConf, error) {
 		log.Println(err)
 		return nil, err
 	}
-
+	var val int64
+	err = util.GetJSONInt64(data, "validators", &val) //Check provided validators
+	if err == nil {
+		if val < 4 || val%2 != 0 {
+			return nil, fmt.Errorf("Invalid number of validators (%d). Validators must be an even number and greater than 3.", val)
+		}
+	}
 	out := new(ArtemisConf)
 	*out = ArtemisConf(util.MergeStringMaps(defaults, data))
 
@@ -28,32 +34,32 @@ func NewConf(data map[string]interface{}) (ArtemisConf, error) {
 }
 
 func GetParams() string {
-    dat, err := ioutil.ReadFile("./resources/artemis/params.json")
-    if err != nil {
-        panic(err)//Missing required files is a fatal error
-    }
-    return string(dat)
+	dat, err := ioutil.ReadFile("./resources/artemis/params.json")
+	if err != nil {
+		panic(err) //Missing required files is a fatal error
+	}
+	return string(dat)
 }
 
 func GetDefaults() string {
-    dat, err := ioutil.ReadFile("./resources/artemis/defaults.json")
-    if err != nil {
-        panic(err)//Missing required files is a fatal error
-    }
-    return string(dat)
+	dat, err := ioutil.ReadFile("./resources/artemis/defaults.json")
+	if err != nil {
+		panic(err) //Missing required files is a fatal error
+	}
+	return string(dat)
 }
 
 func GetServices() []util.Service {
 	return nil
 }
 
-func makeNodeConfig(artemisConf ArtemisConf, identity string, peers string, numNodes int, params map[string]interface{}) (string,error){
+func makeNodeConfig(artemisConf ArtemisConf, identity string, peers string, numNodes int, params map[string]interface{}) (string, error) {
 
 	artConf := map[string]interface{}(artemisConf)
 	artConf["identity"] = identity
 	filler := util.ConvertToStringMap(artConf)
 	filler["peers"] = peers
-	filler["numNodes"] = fmt.Sprintf("%d",numNodes)
+	filler["numNodes"] = fmt.Sprintf("%d", numNodes)
 
 	var validators int64
 	err := util.GetJSONInt64(params, "validators", &validators)
@@ -61,11 +67,11 @@ func makeNodeConfig(artemisConf ArtemisConf, identity string, peers string, numN
 		return "", err
 	}
 
-	filler["validators"] = fmt.Sprintf("%d",validators)
-    dat, err := ioutil.ReadFile("./resources/artemis/artemis-config.toml.mustache")
-    if err != nil {
-        return "",err
-    }
-    data, err := mustache.Render(string(dat), filler)
-    return data,err
+	filler["validators"] = fmt.Sprintf("%d", validators)
+	dat, err := ioutil.ReadFile("./resources/artemis/artemis-config.toml.mustache")
+	if err != nil {
+		return "", err
+	}
+	data, err := mustache.Render(string(dat), filler)
+	return data, err
 }
