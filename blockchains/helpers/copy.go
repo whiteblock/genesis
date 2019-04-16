1cf564b2738dc3b2d18a80e3f87225be6b633b99
n8wb <nate@whiteblock.io>
2019-04-16 20:40:04 +0000

    copy functions

--- blockchains/helpers/copy.go
+++ blockchains/helpers/copy.go
@@ -157,3 +157,37 @@ func CopyBytesToNodeFiles(client *util.SshClient, buildState *state.BuildState, 
 	sem.Release(conf.ThreadLimit)
 	return buildState.GetError()
 }
+
+func CreateConfigs(servers []db.Server, clients []*util.SshClient, buildState *state.BuildState, dest string,
+	fn func(serverNum int, localNodeNum int, absoluteNodeNum int) ([]byte, error)) error {
+
+	sem := semaphore.NewWeighted(conf.ThreadLimit)
+	ctx := context.TODO()
+	node := 0
+	for i, server := range servers {
+		for j := range server.Ips {
+			sem.Acquire(ctx, 1)
+			go func(i int, j int, node int) {
+				defer sem.Release(1)
+				data, err := fn(i, j, node)
+				if err != nil {
+					log.Println(err)
+					buildState.ReportError(err)
+					return
+				}
+				err = SingleCp(clients[i], buildState, j, data, dest)
+				if err != nil {
+					log.Println(err)
+					buildState.ReportError(err)
+					return
+				}
+
+			}(i, j, node)
+			node++
+		}
+	}
+
+	sem.Acquire(ctx, conf.ThreadLimit)
+	sem.Release(conf.ThreadLimit)
+	return buildState.GetError()
+}
