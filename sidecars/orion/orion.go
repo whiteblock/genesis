aad96cd5b95178d01c2a3d2933217e3e9cbe51b4
n8wb <nate@whiteblock.io>
2019-05-04 16:52:19 +0000

    fix and clean orion

--- sidecars/orion/orion.go
+++ sidecars/orion/orion.go
@@ -1,18 +1,15 @@
 package orion
 
 import (
+	"../../blockchains/helpers"
+	"../../blockchains/registrar"
 	"../../db"
 	"../../ssh"
 	"../../testnet"
 	"../../util"
-	// "../../state"
-	"../../blockchains/helpers"
-	"../../blockchains/registrar"
 	"fmt"
+	"github.com/Whiteblock/mustache"
 	"log"
-	// "strings"
-	// "sync"
-	// "github.com/Whiteblock/mustache"
 )
 
 var conf *util.Config
@@ -29,53 +26,45 @@ func init() {
 }
 
 func build(tn *testnet.Adjunct) error {
-	// mux := sync.Mutex{}
 
-	orionconf, err := newConf(tn)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-	fmt.Println(orionconf)
-
-	tn.BuildState.SetBuildSteps(6*tn.LDD.Nodes + 2)
-	tn.BuildState.IncrementBuildProgress()
-
-	err = helpers.AllNodeExecConSC(tn, func(client *ssh.Client, _ *db.Server, node ssh.Node) error {
-		defer tn.BuildState.IncrementBuildProgress()
-		_, err = client.DockerExec(node, "mkdir /orion/data")
+	helpers.AllNodeExecConSC(tn, func(client *ssh.Client, _ *db.Server, node ssh.Node) error { //ignore err
+		_, err := client.DockerExec(node, "mkdir -p /orion/data")
 		return err
 	})
 
-	err = helpers.CreateConfigsSC(tn, "/orion/data/orion.conf",
-		func(node ssh.Node) ([]byte, error) {
-			defer tn.BuildState.IncrementBuildProgress()
-			orionNodeConfig, err := makeNodeConfig(orionconf, node.GetAbsoluteNumber(), tn.LDD)
-			return []byte(orionNodeConfig), err
-		})
+	err := helpers.CreateConfigsSC(tn, "/orion/data/orion.conf", func(node ssh.Node) ([]byte, error) {
+		return makeNodeConfig(node)
+	})
+	if err != nil {
+		return util.LogError(err)
+	}
 
 	err = helpers.AllNodeExecConSC(tn, func(client *ssh.Client, server *db.Server, node ssh.Node) error {
-		defer tn.BuildState.IncrementBuildProgress()
 		_, err := client.DockerExec(node, "bash -c 'cd /orion/data && echo \"\" | orion -g nodeKey'")
 		return err
 	})
 	if err != nil {
-		log.Println(err)
-		return err
+		return util.LogError(err)
 	}
 
-	err = helpers.AllNodeExecConSC(tn, func(client *ssh.Client, server *db.Server, node ssh.Node) error {
-		defer tn.BuildState.IncrementBuildProgress()
+	return helpers.AllNodeExecConSC(tn, func(client *ssh.Client, server *db.Server, node ssh.Node) error {
 		return client.DockerExecdLog(node, "orion /orion/data/orion.conf")
 	})
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	return nil
 }
 
 func add(tn *testnet.Adjunct) error {
 	return nil
+}
+
+func makeNodeConfig(node ssh.Node) ([]byte, error) {
+
+	dat, err := helpers.GetStaticBlockchainConfig(sidecar, "orion.conf.mustache")
+	if err != nil {
+		return nil, util.LogError(err)
+	}
+	data, err := mustache.Render(string(dat), util.ConvertToStringMap(map[string]interface{}{
+		"nodeurl":   fmt.Sprintf("http://%s:8080/", node.GetIP()),
+		"clienturl": fmt.Sprintf("http://%s:8080/", node.GetIP()),
+	}))
+	return []byte(data), err
 }
