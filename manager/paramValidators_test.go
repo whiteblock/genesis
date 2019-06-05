f86e605d26eb09a2058fc982cc415642790f3fe2
renaynay <renenayman11@gmail.com>
2019-06-05 15:19:09 -0700

    test for validate

--- manager/paramValidators_test.go
+++ manager/paramValidators_test.go
@@ -20,7 +20,6 @@ package manager
 
 import (
 	"errors"
-	"fmt"
 	"reflect"
 	"strconv"
 	"testing"
@@ -29,25 +28,52 @@ import (
 	"github.com/whiteblock/genesis/util"
 )
 
-//TODO idk how to test this one
 func Test_validateResources(t *testing.T) {
-	details := &db.DeploymentDetails{
-		ID:           "123",
-		Servers:      []int{4, 5, 6},
-		Blockchain:   "eos",
-		Nodes:        2,
-		Images:       []string{},
-		Params:       map[string]interface{}{},
-		Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
-		Environments: []map[string]string{},
-		Files:        []map[string]string{},
-		Logs:         []map[string]string{},
-		Extras:       map[string]interface{}{},
+	var test = []struct {
+		details  *db.DeploymentDetails
+		expected error
+	}{
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{4, 5, 6},
+				Blockchain:   "eos",
+				Nodes:        100,
+				Images:       []string{},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: "~", Memory: "~", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: errors.New("\"~\" contains invalid character '~'. For node 0"),
+		},
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{4, 5, 6},
+				Blockchain:   "eos",
+				Nodes:        100,
+				Images:       []string{},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: "", Memory: "", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: nil,
+		},
 	}
 
-	validateResources(details)
-
-	fmt.Println()
+	for i, tt := range test {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if !reflect.DeepEqual(validateResources(tt.details), tt.expected) {
+				t.Errorf("returned error of validateResources does not match expected error")
+			}
+		})
+	}
 }
 
 func Test_validateNumOfNodes(t *testing.T) {
@@ -333,6 +359,70 @@ func Test_checkForNilOrMissing(t *testing.T) {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			if !reflect.DeepEqual(checkForNilOrMissing(tt.details), tt.expected) {
 				t.Errorf("returned error from checkForNilOrMissing did not match expected error")
+			}
+		})
+	}
+}
+
+func Test_validate(t *testing.T) {
+	var test = []struct {
+		details  *db.DeploymentDetails
+		expected error
+	}{
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      nil,
+				Blockchain:   "eos",
+				Nodes:        100,
+				Images:       []string{"d", "A", " "},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: errors.New("servers cannot be null"),
+		},
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{},
+				Blockchain:   "geth",
+				Nodes:        100,
+				Images:       []string{"1"},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: errors.New("servers cannot be empty"),
+		},
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{4, 5, 6},
+				Blockchain:   "test_blockchain_doesn't~exist",
+				Nodes:        100,
+				Images:       []string{"~", "", "|"},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: errors.New("strconv.ParseInt: parsing \" \": invalid syntax. For node 0"),
+		},
+	}
+
+	for i, tt := range test {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if !reflect.DeepEqual(validate(tt.details), tt.expected) {
+				t.Errorf("returned error from validate did not match expected value")
 			}
 		})
 	}
