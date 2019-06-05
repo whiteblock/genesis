88297a25fac0adfb94a2a9857bc26e9259f7dafd
renaynay <renenayman11@gmail.com>
2019-06-05 14:32:07 -0700

    test for checkForNilOrMissing

--- manager/paramValidators_test.go
+++ manager/paramValidators_test.go
@@ -225,3 +225,115 @@ func Test_validateBlockchain(t *testing.T) {
 		})
 	}
 }
+
+func Test_checkForNilOrMissing(t *testing.T) {
+	var test = []struct {
+		details  *db.DeploymentDetails
+		expected error
+	}{
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      nil,
+				Blockchain:   "eos",
+				Nodes:        100,
+				Images:       []string{"d", "A", " "},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: errors.New("servers cannot be null"),
+		},
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{},
+				Blockchain:   "geth",
+				Nodes:        100,
+				Images:       []string{"~", "", "|"},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: errors.New("servers cannot be empty"),
+		},
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{4, 5, 6},
+				Blockchain:   "",
+				Nodes:        100,
+				Images:       []string{"~", "", "|"},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: errors.New("blockchain cannot be empty"),
+		},
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{4, 5, 6},
+				Blockchain:   "eos",
+				Nodes:        100,
+				Images:       nil,
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: errors.New("images cannot be null"),
+		},
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{4, 5, 6},
+				Blockchain:   "eos",
+				Nodes:        100,
+				Images:       []string{},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: errors.New("images cannot be empty"),
+		},
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{4, 5, 6},
+				Blockchain:   "eos",
+				Nodes:        100,
+				Images:       []string{"test", "blah"},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: nil,
+		},
+	}
+
+	for i, tt := range test {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if !reflect.DeepEqual(checkForNilOrMissing(tt.details), tt.expected) {
+				t.Errorf("returned error from checkForNilOrMissing did not match expected error")
+			}
+		})
+	}
+}
