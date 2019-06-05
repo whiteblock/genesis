687d516709c4474d9b46618ae98612044632393e
renaynay <renenayman11@gmail.com>
2019-06-05 14:14:39 -0700

    test for validateImages

--- manager/paramValidators_test.go
+++ manager/paramValidators_test.go
@@ -108,7 +108,55 @@ func Test_validateNumOfNodes(t *testing.T) {
 	for i, tt := range test {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			if !reflect.DeepEqual(validateNumOfNodes(tt.details), tt.expected) {
-				t.Errorf("returned error did not match expected error")
+				t.Errorf("returned error of validateNumOfNodes did not match expected error")
+			}
+		})
+	}
+}
+
+func Test_validateImages(t *testing.T) {
+	var test = []struct {
+		details  *db.DeploymentDetails
+		expected error
+	}{
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{4, 5, 6},
+				Blockchain:   "eos",
+				Nodes:        100,
+				Images:       []string{"d", "A", " "},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: nil,
+		},
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{4, 5, 6},
+				Blockchain:   "eos",
+				Nodes:        100,
+				Images:       []string{"~", "", "|"},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: errors.New("\"~\" contains invalid character '~'"),
+		},
+	}
+
+	for i, tt := range test {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if !reflect.DeepEqual(validateImages(tt.details), tt.expected) {
+				t.Errorf("returned error of validateImages did not match expected error")
 			}
 		})
 	}
