fd0be4437dfc859c251cf2a5d1dab13f2141939e
renaynay <renenayman11@gmail.com>
2019-06-05 14:24:42 -0700

    test for validateBlockchain

--- manager/paramValidators_test.go
+++ manager/paramValidators_test.go
@@ -161,3 +161,67 @@ func Test_validateImages(t *testing.T) {
 		})
 	}
 }
+
+func Test_validateBlockchain(t *testing.T) {
+	var test = []struct {
+		details  *db.DeploymentDetails
+		expected error
+	}{
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{4, 5, 6},
+				Blockchain:   "eos",
+				Nodes:        100,
+				Images:       []string{"d", "A", " "},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: nil,
+		},
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{4, 5, 6},
+				Blockchain:   "geth",
+				Nodes:        100,
+				Images:       []string{"~", "", "|"},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: nil,
+		},
+		{
+			details: &db.DeploymentDetails{
+				ID:           "123",
+				Servers:      []int{4, 5, 6},
+				Blockchain:   "test_blockchain_doesn't~exist",
+				Nodes:        100,
+				Images:       []string{"~", "", "|"},
+				Params:       map[string]interface{}{},
+				Resources:    []util.Resources{{Cpus: " ", Memory: " ", Volumes: []string{}, Ports: []string{}}},
+				Environments: []map[string]string{},
+				Files:        []map[string]string{},
+				Logs:         []map[string]string{},
+				Extras:       map[string]interface{}{},
+			},
+			expected: errors.New("\"test_blockchain_doesn't~exist\" contains invalid character '''"),
+		},
+	}
+
+	for i, tt := range test {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if !reflect.DeepEqual(validateBlockchain(tt.details), tt.expected) {
+				t.Errorf("returned error of validateBlockchain does not match expected error")
+			}
+		})
+	}
+}
