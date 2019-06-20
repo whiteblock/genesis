a0dfeb0533ac0d6c58491f275e9a5384d418bcb9
Daniel <choidanielw@gmail.com>
2019-06-20 09:51:20 -0700

    push work

---
+++ protocols/aion/aion.go
@@ -0,0 +1,208 @@
+/*
+	Copyright 2019 whiteblock Inc.
+	This file is a part of the genesis.
+
+	Genesis is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Genesis is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// Package aion handles artemis specific functionality
+package aion
+
+import (
+	"fmt"
+	"regexp"
+	// log "github.com/sirupsen/logrus"
+	"github.com/whiteblock/genesis/db"
+	"github.com/whiteblock/genesis/protocols/helpers"
+	"github.com/whiteblock/genesis/protocols/registrar"
+	"github.com/whiteblock/genesis/ssh"
+	"github.com/whiteblock/genesis/testnet"
+	"github.com/whiteblock/genesis/util"
+	"github.com/whiteblock/mustache"
+	// "reflect"
+	"strings"
+	"sync"
+	"encoding/xml"
+)
+
+var conf *util.Config
+const (
+	blockchain     = "aion"
+	password       = "password"
+)
+
+func init() {
+	conf = util.GetConfig()
+	registrar.RegisterBuild(blockchain, build)
+	registrar.RegisterAddNodes(blockchain, add)
+	registrar.RegisterServices(blockchain, GetServices)
+	registrar.RegisterDefaults(blockchain, helpers.DefaultGetDefaultsFn(blockchain))
+	registrar.RegisterParams(blockchain, helpers.DefaultGetParamsFn(blockchain))
+}
+
+// build builds out a fresh new artemis test network
+func build(tn *testnet.TestNet) error {
+	mux := sync.Mutex{}
+	aionconf, err := newConf(tn.LDD.Params)
+	if err != nil {
+		return util.LogError(err)
+	}
+
+	{
+		/**Create the Password files**/
+		var data string
+		for i := 1; i <= tn.LDD.Nodes; i++ {
+			data += password + "\n" + password
+		}
+		/**Copy over the password file**/
+		err = helpers.CopyBytesToAllNodes(tn, data, "/aion/passwd")
+		if err != nil {
+			return util.LogError(err)
+		}
+	}
+
+	var addresses []string
+
+	err = helpers.AllNewNodeExecCon(tn, func(client ssh.Client, _ *db.Server, node ssh.Node) error {
+		for i:=0;i<node.GetAbsoluteNumber();i++ {
+			output, err := client.DockerExec(node, fmt.Sprintf("echo -e $(cat /aion/password) | /aion/./aion.sh ac -n custom"))
+			if err != nil {
+				return util.LogError(err)
+			}
+			reAddr := regexp.MustCompile(`(?m)A new account has been created:(.{67})`)
+			regAddr := reAddr.FindAllString(output, 1)[0]
+			splitAddr := strings.Split(regAddr, "A new account has been created:")
+			addr := strings.Replace(splitAddr[1], " ", "", -1)
+			fmt.Println(addr)
+			addresses = append(addresses, addr)
+		}
+		return nil
+	})
+	if err != nil {
+		return util.LogError(err)
+	}
+
+	//get permanent node id from auto-generated config.xml
+	err = helpers.AllNewNodeExecCon(tn, func(client ssh.Client, _ *db.Server, node ssh.Node) error {
+		output, err := client.DockerRead(node, fmt.Sprintf("/aion/custom/config/config.xml"), -1)
+		if err != nil {
+			return util.LogError(err)
+		}
+		reNodeID := regexp.MustCompile(`(?m)<id>(.{36})`)
+		regNodeID := reNodeID.FindAllString(output, 1)[0]
+		splitNodeID := strings.Split(regNodeID, "<id>")
+		nodeID := strings.Replace(splitNodeID[1], " ", "", -1)
+		fmt.Println(nodeID)
+		//url := fmt.Sprintf("/ip4/%s/tcp/30333/p2p/%s", node.GetIP(), nodeID)
+		//nodeIDList = append(nodeIDList, url)
+		
+		return nil
+	})
+	if err != nil {
+		return util.LogError(err)
+	}
+
+	err = helpers.AllNewNodeExecCon(tn, func(client ssh.Client, _ *db.Server, node ssh.Node) error {
+		_, err := client.DockerExec(node, fmt.Sprintf("rm /aion/custom/config/config.xml"))
+		if err != nil {
+			return util.LogError(err)
+		}		
+		return nil
+	})
+	if err != nil {
+		return util.LogError(err)
+	}
+
+	err = createGenesisfile(aionconf, tn, addresses)
+	if err != nil {
+		return util.LogError(err)
+	}
+
+
+
+
+	return nil
+}
+
+// Add handles adding a node to the Aion testnet
+// TODO
+func add(tn *testnet.TestNet) error {
+	return nil
+}
+
+// create network configuration files
+// ************************************************************************************
+
+func createGenesisfile(aionconf *AionConf, tn *testnet.TestNet, accounts []string) error {
+	alloc := map[string]map[string]string{}
+	for _, account := range accounts {
+		alloc[account] = map[string]string{
+			"balance": aionconf.InitBalance,
+		}
+	}
+
+	genesis := map[string]interface{}{
+		"initBalance": aionconf.InitBalance,
+		"energyLimit": aionconf.EnergyLimit,
+		"nonce": aionconf.Nonce,
+		"difficulty": aionconf.Difficulty,
+		"timeStamp": aionconf.TimeStamp,
+		"chainId": aionconf.ChainID,
+	}
+
+	genesis["alloc"] = alloc
+	tn.BuildState.Set("alloc", alloc)
+	tn.BuildState.Set("aionconf", aionconf)
+
+	return helpers.CreateConfigs(tn, "/aion/custom/genesis.json", func(node ssh.Node) ([]byte, error) {
+		template, err := helpers.GetBlockchainConfig(blockchain, node.GetAbsoluteNumber(), "chain.json", tn.LDD)
+		if err != nil {
+			return nil, util.LogError(err)
+		}
+
+		data, err := mustache.Render(string(template), util.ConvertToStringMap(genesis))
+		if err != nil {
+			return nil, util.LogError(err)
+		}
+		return []byte(data), nil
+	})
+}
+
+// fix 
+
+func buildConfig(aionconf *AionConf, details *db.DeploymentDetails, wallets []string, passwordFile string, node int) (string, error) {
+
+	dat, err := helpers.GetBlockchainConfig("aion", node, "config.xml.mustache", details)
+	if err != nil {
+		return "", util.LogError(err)
+	}
+	var tmp map[string]interface{}
+
+	raw, err := xml.Marshal(*aionconf)
+	if err != nil {
+		return "", util.LogError(err)
+	}
+
+	err = xml.Unmarshal(raw, &tmp)
+	if err != nil {
+		return "", util.LogError(err)
+	}
+
+	mp := util.ConvertToStringMap(tmp)
+	raw, err = xml.Marshal(wallets)
+	if err != nil {
+		return "", util.LogError(err)
+	}
+	return mustache.Render(string(dat), mp)
+}
---
+++ protocols/aion/conf.go
@@ -0,0 +1,103 @@
+/*
+	Copyright 2019 whiteblock Inc.
+	This file is a part of the genesis.
+
+	Genesis is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Genesis is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package aion
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/whiteblock/genesis/protocols/helpers"
+)
+
+type AionConf struct {
+	IPAddress          string  `xml:"ipAddress"`
+	PeerID             string  `xml:"peerID"`
+	CorsEnabled        bool    `xml:"corsEnabled"`
+	SecureConnect      bool    `xml:"secureConnect"`
+	NRGDefault         int64   `xml:"nrgDefault"`
+	NRGMax             int64   `xml:"nrgMax"`
+	OracleEnabled      bool    `xml:"oracleEnabled"`
+	BlocksQueueMax     int64   `xml:"blocksQueueMax"`
+	ShowStatus         bool    `xml:"showStatus"`
+	ShowStatistics     bool    `xml:"showStatistics"`
+	CompactEnabled     bool    `xml:"compactEnabled"`
+	SlowImport         int64   `xml:"slowImport"`
+	Frequency          int64   `xml:"frequency"`
+	Mining             bool    `xml:"mining"`
+	MineThreads        int64   `xml:"mineThreads"`
+	ExtraData          string  `xml:"extraData"`
+	ClampedDecayUB     int64   `xml:"clampedDecayUB"`
+	ClampedDecayLB     int64   `xml:"clampedDecayLB"`
+	Database           string  `xml:"database"`
+	CheckIntegrity     bool    `xml:"checkIntegrity"`
+	StateStorage       string  `xml:"stateStorage"`
+	Vendor             string  `xml:"vendor"`
+	DBCompression      bool    `xml:"dbCompression"`
+	LogFile            bool    `xml:"logFile"`
+	LogPath            string  `xml:"logPath"`
+	GenLogs            string  `xml:"genLogs`
+	VMLogs             string  `xml:"vmLogs"`
+	APILogs            string  `xml:"apiLogs"`
+	SyncLogs           string  `xml:"syncLogs"`
+	DBLogs             string  `xml:"dbLogs"`
+	ConsLogs           string  `xml:"consLogs"`
+	P2PLogs            string  `xml:"p2plogs"`
+	CacheMax           int64   `xml:"cacheMax"`
+
+	InitBalance        string  `json:"initBalance"`
+	EnergyLimit        int64  `json:"energyLimit"`
+	Nonce              int64  `json:"nonce"`
+	Difficulty         int64  `json:"difficulty"`
+	TimeStamp          int64  `json:"timeStamp"`
+	ChainID            int64  `json:"chainId"`
+}
+
+/**
+ * Fills in the defaults for missing parts,
+ */
+func newConf(data map[string]interface{}) (*AionConf, error) {
+	out := new(AionConf)
+	err := helpers.HandleBlockchainConfig(blockchain, data, out)
+	if err != nil || data == nil {
+		return out, err
+	}
+
+	initBalance, exists := data["initBalance"]
+	if exists && initBalance != nil {
+		switch initBalance.(type) {
+		case json.Number:
+			out.InitBalance = initBalance.(json.Number).String()
+		case string:
+			out.InitBalance = initBalance.(string)
+		default:
+			return nil, fmt.Errorf("incorrect type for initBalance given")
+		}
+	}
+
+	return out, nil
+}
+
+func NewAionConf(data map[string]interface{}) (*AionConf, error) {
+	out := new(AionConf)
+	return out, helpers.HandleBlockchainConfig(blockchain, data, out)
+}
+
+// GetServices returns the services which are used by artemis
+func GetServices() []helpers.Service {
+	return nil
+}
