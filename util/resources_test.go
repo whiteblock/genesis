c6024555d6d59c21f624e87cbb6d2935eb5d4986
renaynay <renenayman11@gmail.com>
2019-07-23 23:01:05 -0500

    latest test

--- util/resources_test.go
+++ util/resources_test.go
@@ -159,3 +159,61 @@ func TestResources_Validate_Unsuccessful(t *testing.T) {
 		})
 	}
 }
+
+func TestResources_ValidateAndSetDefaults_Successful(t *testing.T) {
+	var tests = []struct {
+		res Resources
+	}{
+		{res: Resources{
+			Cpus:    "4",
+			Memory:  "4",
+			Volumes: []string{},
+			Ports:   []string{},
+		}},
+		{res: Resources{
+			Cpus:    "6",
+			Memory:  "49824",
+			Volumes: []string{},
+			Ports:   []string{},
+		}},
+		{res: Resources{
+			Cpus:    "",
+			Memory:  "",
+			Volumes: []string{},
+			Ports:   []string{},
+		}},
+	}
+
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if tt.res.ValidateAndSetDefaults() != nil {
+				t.Error("return value does not match expected value")
+			}
+		})
+	}
+}
+
+func TestResources_GetParsedPortMappings(t *testing.T) {
+	var tests = []struct {
+		res Resources
+	}{
+		{res: Resources{
+			Cpus:    "",
+			Memory:  "",
+			Volumes: []string{},
+			Ports:   []string{},
+		}},
+		{res: Resources{
+			Cpus:    "",
+			Memory:  "",
+			Volumes: []string{},
+			Ports:   []string{},
+		}},
+		{res: Resources{
+			Cpus:    "",
+			Memory:  "",
+			Volumes: []string{},
+			Ports:   []string{},
+		}},
+	}
+}
