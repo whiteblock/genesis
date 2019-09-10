47cdf1e36830f77484eb660a28eea678a05ce33a
renaynay <renenayman11@gmail.com>
2019-09-10 19:19:03 -0400

    merge conflict test

--- protocols/generic/generic_test.go
+++ protocols/generic/generic_test.go
@@ -1,20 +1,99 @@
+/*
+	Copyright 2019 whiteblock Inc.
+	This file is a part of the genesis.
+
+	Genesis is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Genesis is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package generic
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
 )
 
 func TestCreatingNetworkTopologyAll(t *testing.T) {
-	str, err := createPeers(1, map[int]string{0:"a", 1:"b", 2:"c"}, all)
+	var tests = []struct {
+		currentNodeIndex int
+		peerIds map[int]string
+		networkTopology topology
+	}{
+		{
+			currentNodeIndex: 0,
+			peerIds: map[int]string{0:"a", 1:"b", 2:"c"},
+			networkTopology: all,
+		},
+		{
+			currentNodeIndex: 2,
+			peerIds: map[int]string{0:"a", 1:"b", 2:"c"},
+			networkTopology: all,
+		},
+		{
+			currentNodeIndex: 1,
+			peerIds: map[int]string{0:"a", 1:"b", 2:"c"},
+			networkTopology: all,
+		},
+		{
+			currentNodeIndex: 0,
+			peerIds: map[int]string{0:"a", 1:"b", 2:"c"},
+			networkTopology: sequence,
+		},
+		{
+			currentNodeIndex: 2,
+			peerIds: map[int]string{0:"a", 1:"b", 2:"c"},
+			networkTopology: sequence,
+		},
+		{
+			currentNodeIndex: 1,
+			peerIds: map[int]string{0:"a", 1:"b", 2:"c"},
+			networkTopology: sequence,
+		},
+		{
+			currentNodeIndex: 0,
+			peerIds: map[int]string{0:"a", 1:"b", 2:"c"},
+			networkTopology: randomTwo,
+		},
+		{
+			currentNodeIndex: 2,
+			peerIds: map[int]string{0:"a", 1:"b", 2:"c"},
+			networkTopology: randomTwo,
+		},
+		{
+			currentNodeIndex: 1,
+			peerIds: map[int]string{0:"a", 1:"b", 2:"c"},
+			networkTopology: randomTwo,
+		},
+	}
 
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			params, err := createPeers(tt.currentNodeIndex, tt.peerIds, tt.networkTopology)
+			if err != nil {
+				t.Errorf("could not create peers")
+			}
+
+			fmt.Println(params)
+		})
+	}
 }
 
 func TestCreatingNetworkTopologySequence(t *testing.T) {
-	str, err := createPeers(1, map[int]string{0:"a", 1:"b", 2:"c"}, all)
+	//str, err := createPeers(1, map[int]string{0:"a", 1:"b", 2:"c"}, all)
 
 }
 
 func TestCreatingNetworkTopologyRandom(t *testing.T) {
-	str, err := createPeers(1, map[int]string{0:"a", 1:"b", 2:"c"}, all)
-
+	//str, err := createPeers(1, map[int]string{0:"a", 1:"b", 2:"c"}, all)
 }
