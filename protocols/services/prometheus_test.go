8315e54691129050292cdf2cd779c7a14c7c0efe
renaynay <renenayman11@gmail.com>
2019-09-20 12:03:51 -0400

    messing up the format to check travis

--- protocols/services/prometheus_test.go
+++ protocols/services/prometheus_test.go
@@ -60,10 +60,7 @@ func Test_port(t *testing.T) {
 		nodeIndex int
 		expected  string
 	}{
-		{
-			params:    map[string]interface{}{"prometheusInstrumentationPort": []interface{}{"4000"}},
-			nodeIndex: 0,
-			expected:  "4000",
+		{params:    map[string]interface{}{"prometheusInstrumentationPort": []interface{}{"4000"}}, nodeIndex: 0, expected:  "4000",
 		},
 		{
 			params:    map[string]interface{}{"prometheusInstrumentationPort": "3000"},
