13dcc6cfbb9cb943ed7044f2772c7e498edf981c
Antoine Toulme <antoine@lunar-ocean.com>
2019-09-26 10:43:55 -0700

    Resolve circular dependency

--- db/build.go
+++ db/build.go
@@ -22,13 +22,20 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3" //Bring db in
-	"github.com/whiteblock/genesis/protocols/services"
 	"github.com/whiteblock/genesis/util"
 )
 
-/*
-DeploymentDetails represents the data for the construction of a testnet.
-*/
+
+type ServiceDetails struct {
+	Name    string            `json:"name"`
+	Image   string            `json:"image"`
+	Env     map[string]string `json:"env"`
+	Network string            `json:"network"`
+	Ports   []string          `json:"ports"`
+	Volumes []string          `json:"volumes"`
+}
+
+// DeploymentDetails represents the data for the construction of a testnet.
 type DeploymentDetails struct {
 	// ID will be included when it is queried from the database.
 	ID string `json:"id,omitempty"`
@@ -78,7 +85,7 @@ type DeploymentDetails struct {
 	jwt    string
 	kid    string
 
-	Services []services.SimpleService `json:"services"`
+	Services []ServiceDetails `json:"services"`
 }
 
 //SetJwt stores the callers jwt
