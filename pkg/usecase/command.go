07dbc6a2d155f1862edf809b83505c26b355f8b5
n8wb <vanish172@gmail.com>
2019-11-06 12:30:20 -0800

    add configuration for docker

--- pkg/usecase/command.go
+++ pkg/usecase/command.go
@@ -13,45 +13,6 @@
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
-package usecase
-
-import(
-	"github.com/docker/docker/client"
-	"github.com/whiteblock/genesis/pkg/entity"
-	"github.com/whiteblock/genesis/pkg/service"
-)
-
-type CommandUseCase interface {
-	CheckDependenciesExecuted(cmd command.Command) bool
-
-}
-
-type commandUseCase struct {
-}
-
-func NewCommandUseCase() ( CommandUseCase, error) {
-	return commandUseCase{},nil
-}
-
-
-/*
-	Copyright 2019 whiteblock Inc.
-	This file is a part of the genesis.
-
-	Genesis is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	Genesis is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
 	along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
@@ -77,13 +38,13 @@ var (
 type CommandUseCase interface {
 	Run(cmd command.Command) entity.Result
 	RunAsync(cmd command.Command, callback func(cmd command.Command, stat entity.Result))
+	TimeSupplier() int64
 }
 
 
 type commandUseCase struct {
 	service service.CommandService
 	dockerUseCase DockerUseCase
-	
 }
 
 //RunAsync runs a cmdasynchronously and calls the given callback on completion
@@ -132,13 +93,9 @@ func (c commandUseCase) checkSanity(cmd command.Command) (stat entity.Result, ok
 
 func (c commandUseCase) execute(cmd command.Command) entity.Result {
 	if cmd.Timeout == 0 {
-		return c.Runner(context.Background(), cmd.Order)
+		return c.dockerUseCase.Execute(context.Background(),cmd.Order)
 	}
 	ctx, cancelFn := context.WithTimeout(context.Background(), cmd.Timeout)
 	defer cancelFn()
-	return c.Runner(ctx, cmd.Order)
-}
-
-func (c commandUseCase) route(ctx context.Context,cmd command.Command) entity.Result {
-	
+	return  c.dockerUseCase.Runner(ctx, cmd.Order)
 }
---
+++ pkg/usecase/docker.go
@@ -0,0 +1,50 @@
+/*
+	Copyright 2019 whiteblock Inc.
+	This file is a part of the genesis.
+
+	Genesis is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Genesis is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package usecase
+
+import(
+	"context"
+	"github.com/docker/docker/client"
+	"github.com/whiteblock/genesis/pkg/entity"
+	"github.com/whiteblock/genesis/pkg/service"
+)
+
+type DockerUseCase interface{
+	Execute(ctx context.Context,cmd command.Command) entity.Result
+}
+
+type dockerUseCase struct {
+	conf entity.DockerConfig
+}
+
+func NewDockerUseCase(conf entity.DockerConfig) (DockerUseCase, error) {
+	return dockerUseCase{conf:conf},nil
+}
+
+func (duck dockerUseCase) Execute(ctx context.Context,cmd command.Command) entity.Result {
+	cli,err := NewClientWithOpts(
+		client.WithAPIVersionNegotiation(),
+		client.WithHost(cmd.Target.IP),
+		client.WithTLSClientConfig(duck.conf.CACertPath,duck.conf.CertPath,duck.conf.KeyPath)
+	)
+	if err != nil {
+		return entity.Result{Error:err}
+	}
+}
+
