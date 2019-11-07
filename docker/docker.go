7bd524e198fc90a618371d1dc95371b1b48be2b8
renaynay <renenayman11@gmail.com>
2019-11-06 16:02:09 -0800

    latest

---
+++ docker/docker.go
@@ -0,0 +1,30 @@
+/*
+	Copyright 2019 whiteblock Inc.
+	This file is a part of the genesis.
+
+	Genesis is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Genesis is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package docker
+
+import (
+	"github.com/whiteblock/genesis/pkg/entity"
+)
+
+func dockerRunCmdArgs(c entity.Container) (string, error) {
+	var arguments string
+	for _, arg := range c.Args {
+		//todo checks for whether argument is safe (either against a map or string array) -- If safe, then
+	}
+}
