f433f7d10e26eb2824fdb429ce0eba03baae4cb2
n8wb <vanish172@gmail.com>
2019-12-05 11:54:19 -0800

    fix

--- pkg/service/docker.go
+++ pkg/service/docker.go
@@ -90,7 +90,7 @@ func (ds dockerService) CreateClient(host string) (*client.Client, error) {
 	}
 	return client.NewClientWithOpts(
 		client.WithAPIVersionNegotiation(),
-		client.WithHost(host))
+		client.WithHost(host),
 		//client.WithTLSClientConfig(ds.conf.CACertPath, ds.conf.CertPath, ds.conf.KeyPath),
 	)
 }
