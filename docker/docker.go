a727c88a08206be07b29eb9440231de3898192dd
Antoine Toulme <antoine@lunar-ocean.com>
2019-12-12 18:22:49 -0800

    check if the array is there but empty

--- docker/docker.go
+++ docker/docker.go
@@ -120,11 +120,11 @@ func getFlagsFromResources(res util.Resources, index int) (string, error) {
 		out += fmt.Sprintf(" --cpus %s", res.Cpus)
 	}
 
-	if res.Volumes != nil && conf.EnableDockerVolumes {
+	if len(res.Volumes) >= index && conf.EnableDockerVolumes {
 		out += fmt.Sprintf(" -v %s", res.Volumes[index])
 	}
 
-	if res.Ports != nil && conf.EnablePortForwarding {
+	if len(res.Ports) >= index && conf.EnablePortForwarding {
 		out += fmt.Sprintf(" -p %s", res.Ports[index])
 	}
 
