6a095f4baadf7987b9d1cb3dfee1099ad151a52b
Nathaniel Blakely <vanish172@gmail.com>
2020-01-12 06:43:00 -0800

    Local execution (#245)
    * get the rest handler working again
    
    * add local mode option

--- pkg/handler/rest.go
+++ pkg/handler/rest.go
@@ -20,11 +20,13 @@ package handler
 
 import (
 	"encoding/json"
+	"errors"
+	"io/ioutil"
 	"net/http"
-	"sync"
 
 	"github.com/whiteblock/definition/command"
-	"github.com/whiteblock/genesis/pkg/usecase"
+	"github.com/whiteblock/genesis/pkg/entity"
+	"github.com/whiteblock/genesis/pkg/handler/auxillary"
 	util "github.com/whiteblock/utility/utils"
 
 	"github.com/sirupsen/logrus"
@@ -40,97 +42,135 @@ type RestHandler interface {
 	HealthCheck(w http.ResponseWriter, r *http.Request)
 }
 
-type commandWrapper struct {
-	cmd     command.Command
-	retries int
-}
-
 type restHandler struct {
-	cmdChan chan commandWrapper
-	uc      usecase.DockerUseCase
-	once    *sync.Once
-	log     logrus.Ext1FieldLogger
+	aux auxillary.Executor
+	log logrus.Ext1FieldLogger
 }
 
 //NewRestHandler creates a new rest handler
-func NewRestHandler(uc usecase.DockerUseCase, log logrus.Ext1FieldLogger) RestHandler {
+func NewRestHandler(aux auxillary.Executor, log logrus.Ext1FieldLogger) RestHandler {
 	log.Debug("creating a new rest handler")
 	out := &restHandler{
-		cmdChan: make(chan commandWrapper, 200),
-		uc:      uc,
-		once:    &sync.Once{},
-		log:     log,
+		aux: aux,
+		log: log,
 	}
-	go out.start()
 	return out
 }
 
 //AddCommands handles the addition of new commands
-func (rH *restHandler) AddCommands(w http.ResponseWriter, r *http.Request) {
-	var cmds [][]command.Command
-	err := json.NewDecoder(r.Body).Decode(&cmds)
+func (rh *restHandler) AddCommands(w http.ResponseWriter, r *http.Request) {
+	var cmds command.Instructions
+	data, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, util.LogError(err).Error(), 400)
 		return
 	}
-	go func(cmds [][]command.Command) {
-		for _, commands := range cmds {
-			for _, cmd := range commands {
-				rH.cmdChan <- commandWrapper{cmd: cmd, retries: 0}
-			}
-		}
-	}(cmds)
+
+	defer r.Body.Close()
+	err = json.Unmarshal(data, &cmds)
+	if err != nil {
+		http.Error(w, util.LogError(err).Error(), 400)
+		return
+	}
+	go rh.run(&cmds)
 	w.Write([]byte("Success"))
 }
 
+func (rh *restHandler) process(inst *command.Instructions) (result entity.Result) {
+	cmds, err := inst.Peek()
+
+	isLastOne := false
+	if err != nil {
+		if errors.Is(err, command.ErrNoCommands) {
+			rh.log.WithField("error", err).Error("ignoring empty message")
+			return entity.NewIgnoreResult(err)
+		}
+		if !errors.Is(err, command.ErrDone) {
+			rh.log.Error(err)
+			return entity.NewFatalResult(err).InjectMeta(map[string]interface{}{
+				"instructions": *inst,
+			})
+		}
+		isLastOne = true
+	}
+
+	result = rh.aux.ExecuteCommands(cmds)
+
+	if result.IsFatal() {
+		rh.log.WithFields(logrus.Fields{"result": result, "error": result.Error.Error(),
+			"testnet": inst.ID}).Error("execution resulted in a fatal error")
+
+		result = result.InjectMeta(map[string]interface{}{
+			command.OrgIDKey:        inst.OrgID,
+			command.TestIDKey:       inst.ID,
+			command.DefinitionIDKey: inst.DefinitionID,
+		})
+	} else if result.IsTrap() {
+		rh.log.WithField("result", result).Debug("propogating the trap")
+	} else if isLastOne && result.IsSuccess() {
+		if inst.NeverTerminate() {
+			return result.Trap()
+		}
+		rh.log.Debug("creating completion message")
+		result = entity.NewAllDoneResult()
+	} else if result.IsSuccess() {
+		result = entity.NewRequeueResult()
+		rh.log.WithField("remaining", len(inst.Commands)).Debug("creating message for next round")
+		inst.Next()
+	} else if failed, ok := checkPartialFailure(cmds, result); ok {
+		rh.log.WithFields(logrus.Fields{
+			"failed": failed, "succeeded": len(cmds) - len(failed),
+			"result": result,
+		}).Warn("something went partially wrong, requeuing only the commands which failed")
+		inst.PartialCompletion(failed)
+	} else {
+		rh.log.WithField("result", result).Debug("something went wrong, getting kickback message")
+	}
+	return
+}
+
 //HealthCheck handles the reporting of the current health of this service
-func (rH *restHandler) HealthCheck(w http.ResponseWriter, r *http.Request) {
+func (rh *restHandler) HealthCheck(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte("OK"))
 	if err != nil {
-		rH.log.Error(err)
+		rh.log.Error(err)
 	}
 }
 
-//start starts the states inner consume loop
-func (rH *restHandler) start() {
-	rH.once.Do(func() {
-		rH.loop()
-	})
-}
-
-func (rH *restHandler) runCommand(cmd commandWrapper) {
-	res := rH.uc.Run(cmd.cmd)
-	entry := rH.log.WithFields(logrus.Fields{"result": res, "count": cmd.retries})
-
-	if res.IsSuccess() {
-		entry.Trace("a command executed successfully")
-		return
-	}
-	if res.IsFatal() {
-		entry.Error("a command could not execute")
-		return
-	}
-	if res.IsRequeue() {
-		go func() {
-			if cmd.retries < maxRetries {
-				cmd.retries++
-				entry.Info("retrying command")
-				rH.cmdChan <- cmd
-
-			}
-			entry.Error("too many retries for command")
-		}()
-		return
-	}
-}
-
-func (rH *restHandler) loop() {
+func (rh *restHandler) run(inst *command.Instructions) {
+	retries := 0
 	for {
-		cmd, closed := <-rH.cmdChan
-		if !closed {
+		res := rh.process(inst)
+
+		if res.IsAllDone() {
+			rh.log.Info("successfully completed")
 			return
 		}
-		rH.log.WithField("command", cmd).Trace("attempting to run a command")
-		rH.runCommand(cmd)
+		if res.IsFatal() {
+			rh.log.Error("a command could not execute")
+			return
+		}
+
+		if res.IsIgnore() {
+			rh.log.Error("ignoring a message")
+			return
+		}
+		if res.IsTrap() {
+			rh.log.Info("a trap was activated")
+			return
+		}
+
+		if res.IsRequeue() || !res.IsSuccess() {
+			retries++
+			if retries > maxRetries {
+				rh.log.Error("too many retries for command")
+				return
+			}
+			rh.log.Info("retrying command")
+			continue
+		} else {
+			retries = 0
+		}
+		continue
 	}
 }
