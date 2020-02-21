18385011fece97d9177285377713ee7a1703498d
n8wb <vanish172@gmail.com>
2020-02-21 13:27:38 -0800

    fix requeue issue

--- pkg/handler/queue.go
+++ pkg/handler/queue.go
@@ -204,6 +204,8 @@ func (dh deliveryHandler) Process(msg amqp.Delivery) (out amqp.Publishing,
 			"result": result,
 		}).Info("adding the delay field to the header")
 		out.Headers["x-delay"] = int32(result.Delay.Milliseconds())
+	} else {
+		out.Headers["x-delay"] = int32(0)
 	}
 
 	status, err = queue.CreateMessage(stat)
